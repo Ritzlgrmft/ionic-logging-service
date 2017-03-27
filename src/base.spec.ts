4d46eb1c8b6b9e2558e60760645c8fdfaf96364d
Ritzlgrmft <markus.wagner02@web.de>
2017-03-27 20:00:29 +0200

    fix: update dependencies

--- src/base.spec.ts
+++ src/base.spec.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+// tslint:disable:ordered-imports
 import "zone.js/dist/zone";
 import "zone.js/dist/long-stack-trace-zone";
 import "zone.js/dist/proxy";
@@ -6,6 +7,7 @@ import "zone.js/dist/sync-test";
 import "zone.js/dist/jasmine-patch";
 import "zone.js/dist/async-test";
 import "zone.js/dist/fake-async-test";
+// tslint:enable:ordered-imports
 
 import { TestBed } from "@angular/core/testing";
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
