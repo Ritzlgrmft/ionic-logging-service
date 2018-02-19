4831a9930a405aaef12079ef10d94744db225f3c
Ritzlgrmft <markus.wagner02@web.de>
2018-02-19 17:33:43 +0100

    docs(LogLevel): document LogLevel

--- src/log-level.model.ts
+++ src/log-level.model.ts
@@ -2,12 +2,43 @@
  * Logging levels.
  */
 export enum LogLevel {
+	/**
+	 * All events should be logged.
+	 */
 	ALL,
+
+	/**
+	 * A fine-grained debug message, typically capturing the flow through the application.
+	 */
 	TRACE,
+
+	/**
+	 * A general debugging event.
+	 */
 	DEBUG,
+
+	/**
+	 * An event for informational purposes.
+	 */
 	INFO,
+
+	/**
+	 * An event that might possible lead to an error.
+	 */
 	WARN,
+
+	/**
+	 * An error in the application, possibly recoverable.
+	 */
 	ERROR,
+
+	/**
+	 * A severe error that will prevent the application from continuing.
+	 */
 	FATAL,
+
+	/**
+	 * No events will be logged.
+	 */
 	OFF,
 }
