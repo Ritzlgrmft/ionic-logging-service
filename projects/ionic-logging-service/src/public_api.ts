41fe01adc4239fa0023c11a337b2312a1206ffad
Ritzlgrmft <markus.wagner02@web.de>
2019-10-11 18:21:36 +0200

    fix(public api): add missing type definitions
    Closes #14

--- projects/ionic-logging-service/src/public_api.ts
+++ projects/ionic-logging-service/src/public_api.ts
@@ -1,12 +1,18 @@
 /*
- * Public API Surface of ionic-logging-service
+ * Public API Surface of ionic-logging-service.
  */
 
-export * from "./lib/logging.service";
-export * from "./lib/logging-service.module";
+export * from "./lib/ajax-appender.configuration";
 export * from "./lib/ajax-appender.model";
-export * from "./lib/logger.model";
+export * from "./lib/browser-console-appender.configuration";
+export * from "./lib/local-storage-appender.configuration";
+export * from "./lib/local-storage-appender.model";
 export * from "./lib/log-level.converter";
 export * from "./lib/log-level.model";
 export * from "./lib/log-message.model";
+export * from "./lib/logger.model";
+export * from "./lib/logging.service";
 export * from "./lib/logging-service.configuration";
+export * from "./lib/logging-service.module";
+export * from "./lib/memory-appender.configuration";
+export * from "./lib/memory-appender.model";
