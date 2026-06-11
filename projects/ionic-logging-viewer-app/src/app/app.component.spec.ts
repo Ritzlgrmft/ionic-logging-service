482b413adc0e760307651f4d2ad9c2600a689d5d
Markus Wagner <markus.wagner02@web.de>
2026-06-11 10:58:04 +0200

    fix tests for ionic-logging-viewer-app

--- projects/ionic-logging-viewer-app/src/app/app.component.spec.ts
+++ projects/ionic-logging-viewer-app/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect, beforeEach } from "vitest";
 import { TestBed } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
 import { provideZonelessChangeDetection } from "@angular/core";
--- projects/ionic-logging-viewer-app/src/app/home/home.page.spec.ts
+++ projects/ionic-logging-viewer-app/src/app/home/home.page.spec.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect, beforeEach } from "vitest";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
